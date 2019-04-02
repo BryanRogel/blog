86f8143389aeccc1e8696fa88566a501b9c65f56
Abdiel Martinez <abdielantonio13@gmail.com>
2019-04-02 06:49:24 -0600

    pruebas

--- components/prueba.md
+++ components/prueba.md
@@ -1,7 +1,11 @@
 ---
-title:  "SUper titulo shido"
-description: mi descripcion"
-date: 125649849
+title:  "Super titulo shido"
+description: "mi descripcion"
+date: "2019-04-01"
+cover: "/img/logo.png"
+slug: "super-titulo"
+keywords: ['keyword1', 'keyword2']
+tags: ['tag1','taj2']
 ---
 
 # Post body
--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-    main
+    main.article
         Logo
-        vue-disqus(shortname='abdielmartinez' identifier='a1s2d3')
+        //- vue-disqus(shortname='abdielmartinez' identifier='a1s2d3')
 </template>
 
 <script>
-import Logo from '@/components/Logo'
+import Logo from '@/components/ArticuloContenido'
 export default {
     layout:'blog.layout',
     components: {
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style lang="stylus">
-main
+main.article
+    background gray
+    margin auto
     font-family 'Open sans'
+    width 80%
 </style>
