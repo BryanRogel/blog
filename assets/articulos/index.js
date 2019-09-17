8b7e2a13a20ffd3b2fb7b85283286e0a4101efce
Abdiel Martinez <abdielantonio13@gmail.com>
2019-09-16 23:21:24 -0600

    script newpost on process

--- assets/articulos/index.js
+++
@@ -1,5 +0,0 @@
-//lista de articulos disponibles
-
-export const slugs=[
-    "bienvenido"
-]
--- newPost.js
+++ newPost.js
@@ -1,1 +1,111 @@
-console.log('process.argv :', process.argv);
+const fs = require('fs');
+const readFile = (path) => new Promise((resolve, reject)=>{
+    fs.readFile(path,  "utf-8", (err, buf)=>{
+        if(err) return reject(err);
+        
+        resolve(buf.toString());
+    })
+})
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const ask = (text)=> new Promise((resolve, reject)=>{
+    readline.question(text, (answer)=>resolve(answer));
+});
+
+
+const validarSiNo = (res)=>{
+    return res && res =='s' || res == 'S';
+}
+
+const validarContenido = async (contenido, length)=>{
+    const result = await ask(`${contenido}: `);
+
+    if(result.length>length){
+        console.log(`${contenido} sobrepasa la cantidad de caracteres sugeridos(${length}) por ${result.length-length} caracteres`);
+        const res = validarSiNo(await ask('Continuar? s/n: '));
+
+        if(res) return result;
+        
+        return validarContenido(contenido, length);
+    }
+
+    return result;
+}
+
+const getPost= async ()=>{
+    console.log("Detalles del nuevo post");
+    console.log('------------------------');
+
+    const post = {
+        title     : '',
+        description: '',
+        cover    : '',
+        tags       : [],
+        slug       : ''
+    }
+
+    post.title      = await validarContenido('Titulo',65);
+    post.description = await validarContenido('Descripcion', 160);
+    post.cover     = await ask('Imagen de portada: ');
+    const preTags    = await ask("Tags (separados por comas): ");
+
+    let res; 
+    
+    post.tags = preTags.split(',').map(item=>item.trim());
+    
+    res = validarSiNo(await ask('Slug automatico s/n: '));
+    if(res)
+        post.slug = await ask('Slug personalizado: ');
+    else{
+        post.slug = post.title.split(' ').join('-');
+    }
+
+    console.log('------------------------');
+    console.log(post);
+    console.log('------------------------');
+
+    res = validarSiNo(await ask('Informacion correcta? s/n: '));
+    if(res)
+        return getPost();
+    else
+        return post;
+}
+
+const makeHeader=(contenido)=>{
+    const date = new Date();
+    contenido.date = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+    contenido.time = `${date.getHours()}:${date.getMinutes()}`;
+
+    let res = '---';
+
+    for (const key in contenido){
+        if(Array.isArray(contenido[key])){
+            res+=`\n${key}: [`;
+            for (const item of contenido[key])
+                res+=`'${item}',`;
+            res+=']';
+        }else{
+            res+=`\n${key}: "${contenido[key]}"`;
+        }
+    }
+
+    res+='\n---';
+
+    return res;
+}
+
+(async ()=>{
+    
+    const post = await getPost();
+    const metaData = makeHeader(post);
+    fs.writeFileSync(`./assets/articulos/${post.slug}.md`,metaData);
+    
+    
+
+    process.exit();
+})();
